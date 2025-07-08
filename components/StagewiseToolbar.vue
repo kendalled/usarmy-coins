bf4f129066994429e2784bc4761ebedc50997747
Kendall Jackson <kendallkj00@gmail.com>
2025-07-08 17:19:40 -0400

    feat: add stagewise
    Testing to see if it helps with development

---
+++ components/StagewiseToolbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <ClientOnly>
+    <StagewiseToolbarVue />
+  </ClientOnly>
+</template>
+
+<script setup>
+import { StagewiseToolbar as StagewiseToolbarVue } from '@stagewise/toolbar-vue'
+</script>
